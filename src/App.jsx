f2121767a8d7d0b73842340c862a64328cd4109d
muktar01k <adekanyeadewale7@gmail.com>
2025-06-17 09:30:11 +0100

    first push

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import LoginForm from './LoginPage'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+   <>
+    <LoginForm/>
+   </>
+
+  )
+}
+
+export default App
